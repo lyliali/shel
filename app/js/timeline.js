59b4dc839a18f68d09827b59cb88ae3e112da746
Lylia Li <lyliax@gmail.com>
2024-01-18 17:07:21 -0500

    lets try thissss

--- app/js/timeline.js
+++ app/js/timeline.js
@@ -4,18 +4,14 @@
 */
 /* Constants */
 
-
 /* Function Definitions */
 function drawTimeline() {
     let html = "";
     for (let i = 0; i < 16; i++) {
         html = html.concat(`<div id="t${i}"><p>|</p></div>`);
     }
+    html = html.concat(`<span id="year"></span>`);
     document.getElementById("timeline").innerHTML = html;
-}
-
-function highlight() {
-
 }
 
 /* Function Calls */
