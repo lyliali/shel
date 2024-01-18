ab07111d3eed080e2b6f8403492bf222941fd735
Lylia Li <lyliax@gmail.com>
2024-01-18 14:18:49 -0500

    add dedication to page flow

--- app/js/navigation.js
+++ app/js/navigation.js
@@ -5,6 +5,7 @@
 /* Constants */
 const PAGES = [
     "title",
+    "dedication",
     "epigraph",
     "bio",
     "projects",
@@ -17,6 +18,7 @@ const PAGES = [
 
 const SLUGS = [
     "/",
+    "/dedication",
     "/epigraph",
     "/about",
     "/projects",
@@ -27,11 +29,11 @@ const SLUGS = [
     "/index"
 ]
 
-const URL_PREFIX = "https://lylia.li"
+const URL_PREFIX = location.hostname;
 
 /* Function Definitons */
 function documentTitleToArrIndex() {
-    for (let i = 1; i < PAGES.length; i++) {
+    for (let i = 0; i < PAGES.length; i++) {
         if (document.title.startsWith(PAGES[i])) {
             return i;
         }
@@ -40,6 +42,15 @@ function documentTitleToArrIndex() {
 
 function populateNavigation() {
     let current = documentTitleToArrIndex();
+
+    // Nav for title page
+    if (current === 0){
+        let enter = `<a href="${URL_PREFIX.concat(SLUGS[1])}">Enter</a>`;
+        document.getElementById("enter").innerHTML = enter;
+        console.log("done");
+        return;
+    }
+
     let prevUrl = URL_PREFIX.concat(SLUGS[current - 1]);
     let indexUrl = URL_PREFIX.concat(SLUGS[SLUGS.length - 1]);
     let nextUrl = URL_PREFIX.concat(SLUGS[current + 1]);
